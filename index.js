5d7f4c3d018b5e6c40cbc7e7d11cdca304904500
Aaron Freeland <afreeland@gmail.com>
2016-09-27 14:30:10 -0400

    port number comes from env first

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express')
 
 var app =  express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -11,6 +13,6 @@ app.get('/', function(req, res){
     res.render('layout', { title: 'Material Design' });
 });
 
-app.listen(3000, function(){
-    console.log('Memorial Material listening on port 3000');
+app.listen(port, function(){
+    console.log('Memorial Material listening on port ' + port);
 });
